4d1a16420b62336f09a1d243ca48fe8e1f565515
Ben0989 <bleibold2409@gmail.com>
2026-03-06 08:48:03 +0100

    Remove driving stages and tasks from diagrammData.js
    Removed various driving stages and tasks from the diagram data.

--- diagrammData.js
+++ diagrammData.js
@@ -31,42 +31,7 @@ const DIAGRAMM = {
     "Einparken längs",
     "Einparken quer"
   ],
-"Grundstufe": [
-"Besonderheiten beim Einsteigen",
-"Sitz einstellen",
-"Spiegel einstellen",
-"Lenkrad einstellen",
-"Kopfstütze einstellen",
-"Lenkradhaltung",
-"Pedale",
-"Gurt anlegen / anpassen",
-"Motor anlassen"
-],
-
-Aufbaustufe: [
-"Rollen und Schalten",
-"Abbremsen und Schalten",
-"Bremsübung degressiv",
-"Bremsübung Zielbremsung",
-"Gefahrsituationen"
-],
-
-Leistungsstufe: [
-"Fahrstreifenwechsel links",
-"Fahrstreifenwechsel rechts",
-"Überholen",
-"Abbiegen rechts",
-"Abbiegen links"
-],
-
-Grundfahraufgaben: [
-"Rückwärtsfahren",
-"Umkehren",
-"Gefahrenbremsung",
-"Einparken längs",
-"Einparken quer"
-],
-
+  
 Überland: [
 "Angepasste Geschwindigkeit",
 "Abstand vorne",
