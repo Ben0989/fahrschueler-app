5f5c0c31e22416e23f2e2a09a4706d379f76c59d
Ben0989 <bleibold2409@gmail.com>
2026-03-06 08:06:20 +0100

    Update diagrammData.js

--- diagrammData.js
+++ diagrammData.js
@@ -31,4 +31,124 @@ const DIAGRAMM = {
     "Einparken längs",
     "Einparken quer"
   ]
+  const DIAGRAMM = {
+
+Grundstufe: [
+"Besonderheiten beim Einsteigen",
+"Sitz einstellen",
+"Spiegel einstellen",
+"Lenkrad einstellen",
+"Kopfstütze einstellen",
+"Lenkradhaltung",
+"Pedale",
+"Gurt anlegen / anpassen",
+"Motor anlassen"
+],
+
+Aufbaustufe: [
+"Rollen und Schalten",
+"Abbremsen und Schalten",
+"Bremsübung degressiv",
+"Bremsübung Zielbremsung",
+"Gefahrsituationen"
+],
+
+Leistungsstufe: [
+"Fahrstreifenwechsel links",
+"Fahrstreifenwechsel rechts",
+"Überholen",
+"Abbiegen rechts",
+"Abbiegen links"
+],
+
+Grundfahraufgaben: [
+"Rückwärtsfahren",
+"Umkehren",
+"Gefahrenbremsung",
+"Einparken längs",
+"Einparken quer"
+],
+
+Überland: [
+"Angepasste Geschwindigkeit",
+"Abstand vorne",
+"Abstand hinten",
+"Abstand seitlich",
+"Verkehrszeichen",
+"Kreuzungen / Einmündungen",
+"Kurven",
+"Steigungen",
+"Gefälle",
+"Alleen",
+"Überholen",
+"Wild / Tiere"
+],
+
+Autobahn: [
+"Fahrtplanung",
+"Einfahren in BAB",
+"Fahrstreifenwahl",
+"Geschwindigkeit",
+"Abstand vorne",
+"Abstand hinten",
+"Abstand seitlich",
+"Überholen",
+"Schilder / Markierungen",
+"Rastplätze",
+"Unfälle",
+"Dichter Verkehr",
+"Besondere Situationen",
+"Leistungsgrenze",
+"Ablenkung",
+"Konfliktsituationen",
+"Verlassen der BAB"
+],
+
+Dunkelheit: [
+"Beleuchtung Kontrolle",
+"Fernlicht",
+"Beleuchtete Straßen",
+"Unbeleuchtete Straßen",
+"Parken",
+"Blendung",
+"Orientierung"
+],
+
+ReifeTest: [
+"Selbstständiges Fahren innerorts",
+"Selbstständiges Fahren außerorts",
+"Verantwortungsbewusstes Fahren",
+"Testfahrt unter Prüfungsbedingungen",
+"Wiederholung",
+"Leistungsbewertung"
+],
+
+SituativeBausteine: [
+"Reifen",
+"Profiltiefe",
+"Reifendruck",
+"Standlicht",
+"Abblendlicht",
+"Fernlicht",
+"Blinker",
+"Hupe",
+"Bremsleuchte"
+],
+
+Betriebssicherheit: [
+"Motoröl",
+"Kühlmittel",
+"Scheibenwaschflüssigkeit",
+"Warndreieck",
+"Verbandskasten",
+"Bordwerkzeug"
+],
+
+Witterung: [
+"Regen",
+"Aquaplaning",
+"Wind",
+"Schnee",
+"Eis"
+]
 };
