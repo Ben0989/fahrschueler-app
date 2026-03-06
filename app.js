4a0ad58adc802d6caa2b585a5b678c4ceca34d2a
Ben0989 <bleibold2409@gmail.com>
2026-03-06 11:18:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let students = JSON.parse(localStorage.getItem("students") || "[]")
 
+if(!Array.isArray(students)) students = []
+
 let current = null
 
 
 
 function saveDB(){
-
 localStorage.setItem("students", JSON.stringify(students))
-
 }
 
 
@@ -15,14 +15,13 @@ localStorage.setItem("students", JSON.stringify(students))
 function renderList(){
 
 const list = document.getElementById("studentList")
-
 list.innerHTML=""
 
 students.forEach((s,i)=>{
 
 let div=document.createElement("div")
 
-div.innerText=s.name+" "+s.vorname
+div.innerText=(s.name || "") + " " + (s.vorname || "")
 
 div.onclick=()=>openStudent(i)
 
@@ -38,34 +37,31 @@ renderList()
 
 function openStudent(i){
 
+let s = students[i]
+if(!s) return
+
 current=i
 
-let s=students[i]
-
-if(!s.checkboxes) s.checkboxes={}
+if(!s.checkboxes){
+s.checkboxes={}
+}
 
 document.getElementById("studentPanel").classList.remove("hidden")
 
-document.getElementById("studentTitle").innerText=s.name+" "+s.vorname
+document.getElementById("studentTitle").innerText=(s.name || "")+" "+(s.vorname || "")
 
 document.getElementById("info").innerHTML=`
 
-<p><b>Anschrift:</b> ${s.anschrift}</p>
-
-<p><b>Telefon:</b> ${s.telefon}</p>
-
-<p><b>Klasse:</b> ${s.klasse}</p>
-
-<p><b>Beginn:</b> ${s.beginn}</p>
-
-<p><b>Prüfung:</b> ${s.pruefung}</p>
+<p><b>Anschrift:</b> ${s.anschrift || ""}</p>
+<p><b>Telefon:</b> ${s.telefon || ""}</p>
+<p><b>Klasse:</b> ${s.klasse || ""}</p>
+<p><b>Beginn:</b> ${s.beginn || ""}</p>
+<p><b>Prüfung:</b> ${s.pruefung || ""}</p>
 
 `
 
 renderDiagram()
-
 loadDiagram()
-
 updateProgress()
 
 showTab("info")
@@ -77,9 +73,7 @@ showTab("info")
 function showTab(tab){
 
 document.querySelectorAll(".tab").forEach(t=>{
-
 t.classList.add("hidden")
-
 })
 
 document.getElementById(tab).classList.remove("hidden")
@@ -100,6 +94,7 @@ box.className="diagramBox"
 
 let title=document.createElement("h3")
 title.innerText=section
+
 box.appendChild(title)
 
 DIAGRAMM[section].forEach(field=>{
@@ -116,7 +111,9 @@ cb.dataset.field=field
 cb.addEventListener("change",function(){
 
 students[current].checkboxes[field]=cb.checked
+
 saveDB()
+
 updateProgress()
 
 })
@@ -135,6 +132,7 @@ container.appendChild(box)
 }
 
 
+
 function loadDiagram(){
 
 const checkboxes=document.querySelectorAll("#diagramContainer input[type='checkbox']")
@@ -143,7 +141,7 @@ checkboxes.forEach(cb=>{
 
 let field=cb.dataset.field
 
-cb.checked=students[current].checkboxes[field]||false
+cb.checked=students[current].checkboxes[field] || false
 
 })
 
@@ -156,13 +154,10 @@ function updateProgress(){
 const checkboxes=document.querySelectorAll("#diagramContainer input[type='checkbox']")
 
 let total=checkboxes.length
-
 let done=0
 
 checkboxes.forEach(cb=>{
-
 if(cb.checked) done++
-
 })
 
 document.getElementById("progress").innerText="Ausbildungsfelder: "+done+" / "+total
@@ -172,17 +167,13 @@ document.getElementById("progress").innerText="Ausbildungsfelder: "+done+" / "+t
 
 
 document.getElementById("addStudentBtn").onclick=function(){
-
 document.getElementById("addPanel").classList.remove("hidden")
-
 }
 
 
 
 function closeAdd(){
-
 document.getElementById("addPanel").classList.add("hidden")
-
 }
 
 
@@ -192,32 +183,19 @@ function saveStudent(){
 let s={
 
 name:name.value,
-
 vorname:vorname.value,
-
 anschrift:anschrift.value,
-
 geburt:geburt.value,
-
 telefon:telefon.value,
-
 vorbesitz:vorbesitz.value,
-
 klasse:klasse.value,
-
 sehJa:sehJa.checked,
-
 sehNein:sehNein.checked,
-
 beginn:beginn.value,
-
 pruefung:pruefung.value,
-
 checkboxes:{}
 
 }
-
-
 
 students.push(s)
 
