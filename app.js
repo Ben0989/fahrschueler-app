cf05b1735dae23b1b5116a0d829b46cbddc611f4
Ben0989 <bleibold2409@gmail.com>
2026-03-06 08:12:30 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,145 +1,63 @@
-
-let students = JSON.parse(localStorage.getItem("students")||"[]")
-let current=null
-
-function saveDB(){
-localStorage.setItem("students",JSON.stringify(students))
-}
+let students = JSON.parse(localStorage.getItem("students") || "[]")
 
 function renderList(){
-const list=document.getElementById("list")
-list.innerHTML=""
+
+const list = document.getElementById("studentList")
+list.innerHTML = ""
+
 students.forEach((s,i)=>{
-let d=document.createElement("div")
-d.innerText=s.name+" "+s.vorname
-d.onclick=()=>openStudent(i)
-list.appendChild(d)
+
+let div = document.createElement("div")
+div.innerText = s.name + " " + s.vorname
+
+list.appendChild(div)
+
 })
+
 }
 
 renderList()
 
-function showAdd(){
+// + Button öffnet Fenster
+document.getElementById("addStudentBtn").onclick = function(){
+
 document.getElementById("addPanel").classList.remove("hidden")
+
 }
 
+// Fenster schließen
+function closeAdd(){
+
+document.getElementById("addPanel").classList.add("hidden")
+
+}
+
+// Schüler speichern
 function saveStudent(){
 
-let s={
-name:name.value,
-vorname:vorname.value,
-anschrift:anschrift.value,
-geburt:geburt.value,
-telefon:telefon.value,
-vorbesitz:vorbesitz.value,
-klasse:klasse.value,
-sehja:sehja.checked,
-sehnein:sehnein.checked,
-beginn:beginn.value,
-pruefung:pruefung.value,
+let s = {
+
+name: name.value,
+vorname: vorname.value,
+anschrift: anschrift.value,
+geburt: geburt.value,
+telefon: telefon.value,
+vorbesitz: vorbesitz.value,
+klasse: klasse.value,
+sehJa: sehJa.checked,
+sehNein: sehNein.checked,
+beginn: beginn.value,
+pruefung: pruefung.value,
 checkboxes:{}
+
 }
 
 students.push(s)
-saveDB()
-location.reload()
 
-}
+localStorage.setItem("students", JSON.stringify(students))
 
-function openStudent(i){
+closeAdd()
 
-current=i
-let s=students[i]
-
-document.getElementById("studentPanel").classList.remove("hidden")
-document.getElementById("studentTitle").innerText=s.name+" "+s.vorname
-
-document.getElementById("info").innerHTML=`
-<p>${s.anschrift}</p>
-<p>${s.telefon}</p>
-<p>Klasse ${s.klasse}</p>
-`
-
-renderDiagram()
-
-}
-
-function showTab(t){
-
-document.querySelectorAll(".tab").forEach(x=>x.classList.add("hidden"))
-document.getElementById(t).classList.remove("hidden")
-
-}
-
-function renderDiagram(){
-
-let container=document.getElementById("diagramContainer")
-container.innerHTML=""
-
-Object.keys(DIAGRAMM).forEach((section,index)=>{
-
-let box=document.createElement("div")
-box.className="box"
-
-if(index==0)box.classList.add("grund")
-if(index==1)box.classList.add("aufbau")
-if(index==2)box.classList.add("leistung")
-if(index==3)box.classList.add("grundaufgaben")
-
-let h=document.createElement("h3")
-h.innerText=section
-box.appendChild(h)
-
-DIAGRAMM[section].forEach(field=>{
-
-let label=document.createElement("label")
-let cb=document.createElement("input")
-cb.type="checkbox"
-
-cb.checked = students[current].checkboxes[field] || false
-
-cb.onchange=()=>{
-students[current].checkboxes[field]=cb.checked
-updateProgress()
-}
-
-label.appendChild(cb)
-label.append(" "+field)
-
-if(cb.checked)label.classList.add("done")
-
-box.appendChild(label)
-box.appendChild(document.createElement("br"))
-
-})
-
-container.appendChild(box)
-
-})
-
-updateProgress()
-
-}
-
-function saveDiagram(){
-
-saveDB()
-alert("Diagramm gespeichert")
-
-}
-
-function updateProgress(){
-
-let total=0
-let done=0
-
-Object.keys(DIAGRAMM).forEach(s=>{
-DIAGRAMM[s].forEach(f=>{
-total++
-if(students[current].checkboxes[f])done++
-})
-})
-
-document.getElementById("progress").innerText="Ausbildungsfelder: "+done+" / "+total
+renderList()
 
 }
