249ae14b5e7655b1ddeed7b6c11eac8330ad0463
Ben0989 <bleibold2409@gmail.com>
2026-03-06 07:51:52 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ list.innerHTML=""
 students.forEach((s,i)=>{
 let div=document.createElement("div")
 div.className="card"
-div.innerText=s.name+" "+s.vorname+" ("+s.telefon+")"
+div.innerText=s.name+" "+s.vorname
 div.onclick=()=>openStudent(i)
 list.appendChild(div)
 })
@@ -22,12 +22,7 @@ document.getElementById("add").classList.remove("hidden")
 }
 
 function saveStudent(){
-let s={
-name:name.value,
-vorname:vorname.value,
-telefon:telefon.value,
-checkboxes:{}
-}
+let s={name:name.value,vorname:vorname.value,telefon:telefon.value,checkboxes:{}}
 students.push(s)
 saveDB()
 location.reload()
@@ -44,12 +39,16 @@ renderDiagram()
 function renderDiagram(){
 const container=document.getElementById("diagramm")
 container.innerHTML=""
-container.className="grid"
 
-Object.keys(DIAGRAMM).forEach(section=>{
+Object.keys(DIAGRAMM).forEach((section,index)=>{
 
 let box=document.createElement("div")
 box.className="box"
+
+if(index==0)box.classList.add("grund")
+if(index==1)box.classList.add("aufbau")
+if(index==2)box.classList.add("leistung")
+if(index==3)box.classList.add("grundaufgaben")
 
 let h=document.createElement("h3")
 h.innerText=section
@@ -63,17 +62,8 @@ cb.type="checkbox"
 
 cb.checked = students[current].checkboxes[field] || false
 
-cb.onchange=()=>{
-students[current].checkboxes[field]=cb.checked
-saveDB()
-updateProgress()
-if(cb.checked){label.classList.add("done")}else{label.classList.remove("done")}
-}
-
 label.appendChild(cb)
 label.append(" "+field)
-
-if(cb.checked)label.classList.add("done")
 
 box.appendChild(label)
 box.appendChild(document.createElement("br"))
@@ -87,7 +77,21 @@ container.appendChild(box)
 updateProgress()
 }
 
+document.getElementById("saveDiagramBtn").onclick=function(){
+
+document.querySelectorAll("#diagramm input[type=checkbox]").forEach(cb=>{
+let text=cb.parentElement.innerText.trim()
+students[current].checkboxes[text]=cb.checked
+})
+
+saveDB()
+alert("Diagramm gespeichert")
+updateProgress()
+
+}
+
 function updateProgress(){
+
 let total=0
 let done=0
 
