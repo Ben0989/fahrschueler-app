8b533af97c548f7a580b015ff250ccd01df3d172
Ben0989 <bleibold2409@gmail.com>
2026-03-06 12:21:14 +0100

    Update diagrammData.js

--- diagrammData.js
+++ diagrammData.js
@@ -8,43 +8,137 @@ const DIAGRAMM = {
     "Lenkradhaltung",
     "Pedale",
     "Gurt anlegen / anpassen",
-    "Motor anlassen"
+    "Motor anlassen",
+    "Anfahr-/Anhalteübungen"
+  "Schaltübungen": [
+    "hoch": [
+    "1-2",
+    "2-3"
+    "3-4" 
+    ]
+    "runter": [
+     "4-3",
+     "3-2",
+     "2-1"
+    ]
+  "Lenkübungen"
   ],
   "Aufbaustufe": [
     "Rollen und Schalten",
     "Abbremsen und Schalten",
     "Bremsübung degressiv",
     "Bremsübung Zielbremsung",
-    "Gefahrsituationen"
+    "Bremsübung Gefahrsituationen"
+   "Gefälle": [
+    "Anhalten",
+    "Anfahren",
+    "Rückwärts",
+    "Sichern",
+    "Schalten"
+    ]
+    "Steigung": [
+     "Anhalten",
+     "Anfahren",
+     "Rückwärts",
+     "Sichern",
+     "Schalten"
+    ]]
+    "Tastgeschwindigkeit",
+    "Bedienungs- und Kontrolleinrichtungen",
+    "Örtliche Besonderheiten"
   ],
   "Leistungsstufe": [
+    "Fahrbahnbenutzung": [
+     "Einordnen",
+     "Makierungen"
+    ]
     "Fahrstreifenwechsel links",
     "Fahrstreifenwechsel rechts",
-    "Überholen",
+    "Vorbeifahren/Überholen",
     "Abbiegen rechts",
-    "Abbiegen links"
+    "Abbiegen links",
+    "Abbiegen mehrspurig",
+    "Radweg",
+    "Straßenbahn",
+    "Sonderfahrstreifen",
+    "Einbahnstraße"
+    "Vorfahrt": [
+     "rechts vor links",
+     "Polizeibeamte",
+     "Grünpfeil",
+     "Grünpfeil-Schild"
+    ]
+    "Geschwindigkeit/Abstand"
+    "Situationen mit anderen Vekehrsteilnehmern": [
+      "Fußgängerüberwege",
+      "Öffentl. Verkehrsmittel",
+      "Ältere/Behinderte",
+      "Einbahnstr./Radfahrer",
+      "Kinder",
+      "Schulbus",
+      "Radfahrer/Mofa",
+      "Verk.-beruh. Bereich"
+      ]
+    "Schwierige Verkehrsführung",
+    "Engpass",
+    "Kreisverkehr",
+    "Bahnübergang (warten)"
+    "Kritische Verkehrssituation": [
+      "Hauptverkehrszeiten",
+      "Part. Verhalten (Kommu., Verzicht auf Vorfahrt),
+      "Schwung nutzen"
+    ]
+    "Fußgänger Schutzbereich"
   ],
   "Grundfahraufgaben": [
     "Rückwärtsfahren",
     "Umkehren",
-    "Gefahrenbremsung",
-    "Einparken längs",
+    "Gefahrenbremsung"
+    "Einparken längs": [
+    "Vorwärts": [
+    "Links",
+    "Rechts"
+    ]
+    "Rückwärts": [
+    "Links",
+    "Rechts"
+    ]]
+
     "Einparken quer"
+     "Vorwärts": [
+      "Links",
+      "Rechts"
+      ]
+    "Rückwärts": [
+     "Links",
+     "Rechts"
+    ]]
   ],
   
 "Überland": [
-"Angepasste Geschwindigkeit",
+"Angepasste Geschwindigkeit/Gangwahl",
 "Abstand vorne",
 "Abstand hinten",
 "Abstand seitlich",
+"Beobachtung/Spiegel",
 "Verkehrszeichen",
 "Kreuzungen / Einmündungen",
 "Kurven",
 "Steigungen",
 "Gefälle",
 "Alleen",
-"Überholen",
-"Wild / Tiere"
+"Überholen"
+"Besondere Situationen: [
+ "Liegenbleiben u Absichern"
+ "Einfahren in Ortschaften",
+ "Fußgänger",
+ "Wild / Tiere"
+]
+"Besondere Anforderungen": [
+  "Leistungsgrenze",
+  "Ablenkung (z.B. Radio)",
+  "Orientierung"
+]
 ],
 
 "Autobahn": [
@@ -57,61 +151,138 @@ const DIAGRAMM = {
 "Abstand seitlich",
 "Überholen",
 "Schilder / Markierungen",
-"Rastplätze",
-"Unfälle",
-"Dichter Verkehr",
-"Besondere Situationen",
-"Leistungsgrenze",
-"Ablenkung",
-"Konfliktsituationen",
+"Vorbeifahren/Anschlussstellen",
+"Rast-, Parkplätze, Tankstellen",
+"Verhalten bei Unfällen",
+"Dichter Verkehr/Stau",
+"Besondere Situationen"
+"Besondere Anforderungen": [
+ "Leistungsgrenze",
+ "Ablenkung",
+ "Konfliktsituationen"
+]
 "Verlassen der BAB"
 ],
 
 "Dunkelheit": [
-"Beleuchtung Kontrolle",
-"Fernlicht",
+"Beleuchtung": [ 
+ "Kontrolle",
+ "Einstellen",
+ "Benutzung",
+ "Fernlicht"
+]
 "Beleuchtete Straßen",
 "Unbeleuchtete Straßen",
-"Parken",
-"Blendung",
-"Orientierung"
+"Parken"
+"Besondere Situationen": [
+  "Schlechte Witterung",
+  "Bahnübergänge",
+  "Tiere",
+  "unbeleuchtete Verkehrsteilnehmer"
+  ]
+"Besondere Anforderungen": [
+  "Blendung",
+  "Orientierung"
+  ]
+"Abschlussbesprechung"
 ],
 
 "ReifeTest": [
 "Selbstständiges Fahren innerorts",
 "Selbstständiges Fahren außerorts",
 "Verantwortungsbewusstes Fahren",
-"Testfahrt unter Prüfungsbedingungen",
-"Wiederholung",
+"Testfahrt unter Prüfungsbedingungen": [
+  "FAKT",
+  "andere"
+  ]
+"Wiederholung/Vertiefung",
 "Leistungsbewertung"
 ],
 
 "SituativeBausteine": [
-"Reifen",
-"Profiltiefe",
-"Reifendruck",
-"Standlicht",
-"Abblendlicht",
-"Fernlicht",
-"Blinker",
-"Hupe",
-"Bremsleuchte"
+"Reifen": [
+  "Beschädigung",
+  "Profiltiefe",
+  "Reifendruck"
+  ]
+"Scheinwerfer, Leuchten, Blinker, Hupe": [
+  "Einschalten",
+  "Ausschalten"
+  ]
+"Funktion prüfen": [
+  "Standlicht",
+  "Abblendlicht",
+  "Fernlicht",
+  "Schlussleuchten m. Kennzeichenbeleuchtung",
+  "Nebelschlussleuchte",
+  "Warnblinkanlage"
+  "Blinker",
+  "Hupe",
+  "Bremsleuchte"
+]
+"Kontrollleuchten benennen",
+"Rückstrahler Vorhanden",
+"Rückstrahler beschädigt",
+"Lenkung": [
+  "Lenkschloss entriegeln",
+  "überprüfen des Lenkspiels"
+]
+"Funtionsprüfung der Bremsen": [
+  "Betriebsbremse",
+  "Feststellbremse"
+]
 ],
-
-"Betriebssicherheit": [
+"Fahrassistenzsysteme": [
+  "Einschaten",
+  "Ausschalten",
+  "Anwenden"
+  ]
+],
+"Heizung und Lüftung": [
+"Bedienen der Aggregate": [
+  "Heizung",
+  "Lüftung",
+  "Klimaanlage",
+  "Heckscheibenheizung",
+  "Beheizte Sonderausstattung"
+]
+"Energiesparende Nutzung" [
+  "Keine unnötigen Verbraucher",
+  "Rechtzeituges Abschalten"
+]
+],
+"Betriebs- und Verkehrssicherheit": [
 "Motoröl",
 "Kühlmittel",
 "Scheibenwaschflüssigkeit",
 "Warndreieck",
 "Verbandskasten",
-"Bordwerkzeug"
+"Bordwerkzeug",
+"Zustzliche Ausrüstung",
+"Tanken": [
+  "Warndreieck",
+  "Boardwerkzeug",
+  "Verbandskasten",
+  "zusätzliche Ausrüstung"
+  ]
+  "Aussenkontrolle (Schäden, Sauberkeit)": [
+    "Scheiben/Wischer",
+    "Spiegel",
+    "Kennzeichen (HU)",
+    "Beleuhtung"
+  ]
+"Bremsen",
+"Ladungssicherung",
+"Ldungskennzeichnung"
 ],
 
 "Witterung": [
-"Regen",
-"Aquaplaning",
-"Wind",
-"Schnee",
+"Lüftung",
+"Scheibenwischer/-wasser",
+"Regen, Sprühnebel",
+"Wasserlachen/Aquaplaning",
+"Wind, Sturm, Böen",
+"Schnee und Matsch",
 "Eis"
 ]
 };
