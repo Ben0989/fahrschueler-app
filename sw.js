48839550723cb80962d8ae53ecca31374f4179ec
Ben0989 <bleibold2409@gmail.com>
2026-03-16 12:15:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,101 @@
-
-const CACHE_NAME = "fs-app-v1"
+const CACHE_NAME = "fs-app-cache-v3"
 
 const ASSETS = [
 "./",
 "./index.html",
 "./style.css",
+
 "./js/app.js",
 "./js/students.js",
 "./js/sonderfahrten.js",
 "./js/fahrten.js",
+"./js/diagrammData.js",
+"./js/backup.js",
+
 "./icons/icon-192.png",
 "./icons/icon-512.png"
 ]
 
-self.addEventListener("install", e => {
+/* =============================
+INSTALL
+============================= */
 
-e.waitUntil(
-caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener("install", event => {
+
+self.skipWaiting()
+
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(ASSETS))
 )
 
 })
 
-self.addEventListener("fetch", e => {
+/* =============================
+ACTIVATE
+============================= */
 
-e.respondWith(
-caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+
+caches.keys().then(keys => {
+
+return Promise.all(
+
+keys.map(key => {
+
+if(key !== CACHE_NAME){
+
+return caches.delete(key)
+
+}
+
+})
+
+)
+
+})
+
+)
+
+self.clients.claim()
+
+})
+
+/* =============================
+FETCH
+============================= */
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response => {
+
+if(response){
+return response
+}
+
+return fetch(event.request)
+.then(networkResponse => {
+
+if(!networkResponse || networkResponse.status !== 200){
+return networkResponse
+}
+
+let responseClone = networkResponse.clone()
+
+caches.open(CACHE_NAME)
+.then(cache => cache.put(event.request, responseClone))
+
+return networkResponse
+
+})
+
+})
+
 )
 
 })
